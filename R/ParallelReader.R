391760b768dcab8e1c14108ed22c0ebdb448c842
Tennismylife <infotennismylife@gmail.com>
2021-05-04 03:47:59 +0200

    Fix

--- R/ParallelReader.R
+++ R/ParallelReader.R
@@ -1,3 +1,15 @@
+#' @title Read online database
+#'
+#' @description with this function you can retrieve all ATP matches lively updated
+#'
+#' @param
+#'
+#' @return NULL
+#'
+#' @examples  ParallelReader()
+#'
+#' @export ParallelReader
+
 ### Functions to interrogate the databases of matches
 
 library(data.table)
