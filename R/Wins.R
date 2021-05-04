fa7798d9836af368860f90a3b4be8c752bff2763
Tennismylife <infotennismylife@gmail.com>
2021-05-04 04:06:58 +0200

    Fix

---
+++ R/Wins.R
@@ -0,0 +1,28 @@
+#' @title ATP winning matches record
+#'
+#' @description ATP winning matches record
+#'
+#' @param
+#'
+#' @return res
+#'
+#' @examples  WinsOverall()
+#'
+#' @export WinsOverall
+
+library(stringr)
+
+WinsOverall <- function() {
+
+  ## drop walkover matches (not countable)
+  require(stringr)
+  db <- db[!db$score=="W/O" & !db$score=="DEF" & !str_detect(db$score, "WEA") & !str_detect(db$score, "ABN")]
+
+  ## count occurrences of won matches
+  res <- db[,.N, by=winner_name]
+
+  ## order by decreasing
+  setorder(res, -N, na.last=FALSE)
+
+  res
+}
